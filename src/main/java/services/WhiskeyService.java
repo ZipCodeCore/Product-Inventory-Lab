1fe9a1794fe2aa4bc2ea46ec560351a4a4819d60
vaidehi4321 <56602405+vaidehi4321@users.noreply.github.com>
2019-11-29 12:44:12 -0500

    whiskey complete

--- src/main/java/services/WhiskeyService.java
+++ src/main/java/services/WhiskeyService.java
@@ -15,7 +15,28 @@ public class WhiskeyService {
         joyInventory.add(whiskey);
         return whiskey;
     }
-    public Whiskey findAll(){
+    public Whiskey findWhiskey(int id){
+        for (int i = 0; i < joyInventory.size(); i++) {
+            if (joyInventory.get(i).getId() == id)
+                return joyInventory.get(i);
+        }
         return null;
+    }
+    public Whiskey[] findAll() {
+        Whiskey[] sneakersArr=new Whiskey[joyInventory.size()];
+        for(int i=0; i< joyInventory.size(); i++){
+            sneakersArr[i] = joyInventory.get(i);
+        }
+
+        return sneakersArr;
+    }
+    public boolean delete(int id) {
+        for(int i=0; i<joyInventory.size();i++){
+            if(joyInventory.get(i).getId()==id){
+                joyInventory.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
 }
