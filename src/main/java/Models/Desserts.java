c95fb989c9e07d39fcb9fa03ae1c23daa186da53
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-13 18:06:25 -0500

    dessert const tests

--- src/main/java/Models/Desserts.java
+++ src/main/java/Models/Desserts.java
@@ -2,9 +2,17 @@ package Models;
 
 public class Desserts {
     private String name;
-    private boolean servedCold;
     private double price;
+    private boolean servedCold;
     private boolean currentlyOnMenu;
+
+    public Desserts(String name, double price, boolean servedCold, boolean currentlyOnMenu){
+        this.name = name;
+        this.price = price;
+        this.servedCold = servedCold;
+        this.currentlyOnMenu = currentlyOnMenu;
+
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -17,5 +25,19 @@ public class Desserts {
     }
     public double getPrice() {
         return price;
+    }
+
+    public void setServedCold(boolean servedCold) {
+        this.servedCold = servedCold;
+    }
+    public boolean getServedCold() {
+        return servedCold;
+    }
+
+    public void setcurrentlyOnMenu(boolean currentlyOnMenu) {
+        this.currentlyOnMenu = currentlyOnMenu;
+    }
+    public boolean getcurrentlyOnMenu() {
+        return currentlyOnMenu;
     }
 }
--- src/test/java/Models/DessertsTest.java
+++ src/test/java/Models/DessertsTest.java
@@ -4,18 +4,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DessertsTest {
-    @Test
-    public void setNameTest() {
-        String expected = "brownie";
-        Desserts testDessert = new Desserts();
-        testDessert.setName(expected);
-        Assertions.assertEquals(expected, testDessert.getName());
-    }
-    @Test
-    public void setPriceTest() {
-        double expected = 3.00;
-        Desserts testDessert = new Desserts();
-        testDessert.setPrice(expected);
-        Assertions.assertEquals(expected, testDessert.getPrice());
-    }
+//    @Test
+//    public void setNameTest() {
+//        String expected = "brownie";
+//        Desserts testDessert = new Desserts();
+//        testDessert.setName(expected);
+//        Assertions.assertEquals(expected, testDessert.getName());
+//    }
+//    @Test
+//    public void setPriceTest() {
+//        double expected = 3.00;
+//        Desserts testDessert = new Desserts();
+//        testDessert.setPrice(expected);
+//        Assertions.assertEquals(expected, testDessert.getPrice());
+//    }
+@Test // (1)
+public void constructorTest(){
+    String exname = "icecream";
+    double exprice = 4.00;
+    boolean exservedCold = true;
+    boolean excurrentlyOnMenu = false;
+
+    Desserts testDessert = new Desserts(exname, exprice, exservedCold,
+            excurrentlyOnMenu);
+
+
+    Assertions.assertEquals(exname, testDessert.getName());
+    Assertions.assertEquals(exprice, testDessert.getPrice());
+    Assertions.assertEquals(exservedCold, testDessert.getServedCold());
+    Assertions.assertEquals(excurrentlyOnMenu, testDessert.getcurrentlyOnMenu());
+
+}
 }
