4e159ab89ef2b4537a0cfc678f21a2328386b243
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-13 17:20:33 -0500

    tc get set tests

--- src/main/java/Models/TradingCards.java
+++ src/main/java/Models/TradingCards.java
@@ -5,4 +5,17 @@ public class TradingCards {
     private int rarity;
     boolean isFromStartedDeck;
     private int price;
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+    public String getGame() {
+        return game;
+    }
+    public void setRarity(int rarity) {
+        this.rarity = rarity;
+    }
+    public int getRarity() {
+        return rarity;
+    }
 }
--- src/test/java/Models/TradingCardTest.java
+++ src/test/java/Models/TradingCardTest.java
@@ -1,4 +1,21 @@
 package Models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TradingCardTest {
+    @Test
+    public void setGameTest() {
+        String expected = "pokemon";
+        TradingCards testGame = new TradingCards();
+        testGame.setGame(expected);
+        Assertions.assertEquals(expected, testGame.getGame());
+    }
+    @Test
+    public void setRarityTest() {
+        int expected = 5;
+        TradingCards testRarity = new TradingCards();
+        testRarity.setRarity(expected);
+        Assertions.assertEquals(expected, testRarity.getRarity());
+    }
 }
