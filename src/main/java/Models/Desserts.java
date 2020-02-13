12a89c224f070fe537709e989e28ad6cc30aa732
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-13 17:29:42 -0500

    dessert get set tests

--- src/main/java/Models/Desserts.java
+++ src/main/java/Models/Desserts.java
@@ -1,4 +1,21 @@
 package Models;
 
 public class Desserts {
+    private String name;
+    private boolean servedCold;
+    private double price;
+    private boolean currentlyOnMenu;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
--- src/test/java/Models/DessertsTest.java
+++ src/test/java/Models/DessertsTest.java
@@ -1,4 +1,21 @@
 package Models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DessertsTest {
+    @Test
+    public void setNameTest() {
+        String expected = "brownie";
+        Desserts testDessert = new Desserts();
+        testDessert.setName(expected);
+        Assertions.assertEquals(expected, testDessert.getName());
+    }
+    @Test
+    public void setPriceTest() {
+        double expected = 3.00;
+        Desserts testDessert = new Desserts();
+        testDessert.setPrice(expected);
+        Assertions.assertEquals(expected, testDessert.getPrice());
+    }
 }
