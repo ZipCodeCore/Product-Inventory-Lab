394314f1beaed36ecb5c0b59f4d98b48c9bcae5c
Emma Beech <ebeech@zipcodes-MacBook-Pro.local>
2020-02-13 17:53:46 -0500

    tc const tests

--- src/main/java/Models/TradingCards.java
+++ src/main/java/Models/TradingCards.java
@@ -3,8 +3,15 @@ package Models;
 public class TradingCards {
     private  String game;
     private int rarity;
-    boolean isFromStartedDeck;
-    private int price;
+    private boolean isFromStartedDeck;
+    private double price;
+
+    public TradingCards(String game, int rarity, boolean isFromStartedDeck, double price){
+        this.game = game;
+        this.rarity = rarity;
+        this.isFromStartedDeck = isFromStartedDeck;
+        this.price = price;
+    }
 
     public void setGame(String game) {
         this.game = game;
@@ -17,5 +24,19 @@ public class TradingCards {
     }
     public int getRarity() {
         return rarity;
+    }
+
+    public void setFromStarterDeck(boolean isFromStartedDeck) {
+        this.isFromStartedDeck = isFromStartedDeck;
+    }
+    public boolean getFromStarterDeck() {
+        return isFromStartedDeck;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
     }
 }
--- src/test/java/Models/TradingCardTest.java
+++ src/test/java/Models/TradingCardTest.java
@@ -4,18 +4,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TradingCardTest {
-    @Test
-    public void setGameTest() {
-        String expected = "pokemon";
-        TradingCards testGame = new TradingCards();
-        testGame.setGame(expected);
-        Assertions.assertEquals(expected, testGame.getGame());
-    }
-    @Test
-    public void setRarityTest() {
-        int expected = 5;
-        TradingCards testRarity = new TradingCards();
-        testRarity.setRarity(expected);
-        Assertions.assertEquals(expected, testRarity.getRarity());
+//    @Test
+//    public void setGameTest() {
+//        String expected = "pokemon";
+//        TradingCards testGame = new TradingCards();
+//        testGame.setGame(expected);
+//        Assertions.assertEquals(expected, testGame.getGame());
+//    }
+//    @Test
+//    public void setRarityTest() {
+//        int expected = 5;
+//        TradingCards testRarity = new TradingCards();
+//        testRarity.setRarity(expected);
+//        Assertions.assertEquals(expected, testRarity.getRarity());
+//    }
+    @Test // (1)
+    public void constructorTest(){
+          String exgame = "DnD";
+         int exrarity = 6;
+         boolean exisFromStartedDeck = false;
+         double exprice = 15.00;
+
+        TradingCards testCard = new TradingCards(exgame, exrarity, exisFromStartedDeck,
+                exprice);
+
+
+        Assertions.assertEquals(exgame, testCard.getGame());
+        Assertions.assertEquals(exrarity, testCard.getRarity());
+        Assertions.assertEquals(exisFromStartedDeck, testCard.getFromStarterDeck());
+        Assertions.assertEquals(exprice, testCard.getPrice());
+
     }
 }
+
