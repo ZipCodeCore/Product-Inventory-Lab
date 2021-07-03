ebb12cee94b9fda4f7099d091092df857496f46e
Zach <zach@zipcodes-MacBook-Pro-6.local>
2021-07-03 11:18:16 -0400

    added tests and constructor for Whiskey

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -9,11 +9,68 @@ public class Sneaker {
     private int qty;
     private Float price;
 
+    public Sneaker(int id, String name, String brand, String sport, int qty, float price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.sport = sport;
+        this.qty = qty;
+        this.price = price;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public Float getSize() {
+        return size;
+    }
+
+    public void setSize(Float size) {
+        this.size = size;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
     }
 }
--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -8,4 +8,70 @@ public class Whiskey {
     private Float size;
     private int qty;
     private Float price;
+
+    public Whiskey(int id, String name, String brand, int proof, Float size, int qty, Float price){
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.proof = proof;
+        this.size = size;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getProof() {
+        return proof;
+    }
+
+    public void setProof(int proof) {
+        this.proof = proof;
+    }
+
+    public Float getSize() {
+        return size;
+    }
+
+    public void setSize(Float size) {
+        this.size = size;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
