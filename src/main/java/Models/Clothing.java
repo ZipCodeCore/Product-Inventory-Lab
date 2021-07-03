7517b7cd0aaf375df38d084fb58acdb8fa1d1983
sitarameez <sitarablossom@gmail.com>
2021-07-03 12:09:51 -0400

    added properties to clothing class

--- src/main/java/Models/Clothing.java
+++ src/main/java/Models/Clothing.java
@@ -1,4 +1,10 @@
 package Models;
 
 public class Clothing {
+    private int id;
+    private String name;
+    private String brand;
+    private int size;
+    private int quantity;
+    private float price;
 }
