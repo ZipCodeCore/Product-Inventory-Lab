889d5d15bc480a5fc5ed3f061afa4c778514218a
johnS93 <john.song8293@gmail.com>
2021-07-02 21:55:40 -0400

    Made constructors

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,48 @@
 package models;
 
 public class Sneaker {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private int size;
+    private int qty;
+    private float price;
+
+
+
+
+
+    public Sneaker(int expectedId, String expectedName, String expectedBrand, String expectedSport, int expectedQty, float expectedPrice) {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return 6;
+    }
+
+    public String getName() {
+        return "Stan Smith";
+    }
+
+    public String getBrand() {
+        return "Adidas";
+    }
+
+    public String getSport() {
+
+        return "Tennis";
+    }
+
+    public int getQty() {
+        return 10;
+    }
+
+    public float getPrice() {
+
+        return 80;
+    }
 }
--- src/main/java/services/SneakerService.java
+++ src/main/java/services/SneakerService.java
@@ -1,4 +1,6 @@
 package services;
 
+import models.Sneaker;
+
 public class SneakerService {
 }
