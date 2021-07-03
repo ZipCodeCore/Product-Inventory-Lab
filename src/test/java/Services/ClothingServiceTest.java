df138b4a25457c810be2680a0483a0e300e31a28
sitarameez <sitarablossom@gmail.com>
2021-07-03 18:27:41 -0400

    completed clothing section 2

--- src/test/java/Services/ClothingServiceTest.java
+++ src/test/java/Services/ClothingServiceTest.java
@@ -37,12 +37,52 @@ class ClothingServiceTest {
         int actualQuantity = testClothing.getQuantity();
         float actualPrice = testClothing.getPrice();
 
-        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        //Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
         Assertions.assertEquals(expectedName, actualName);
         Assertions.assertEquals(expectedBrand, actualBrand);
         Assertions.assertEquals(expectedSize, actualSize);
         Assertions.assertEquals(expectedQuantity, actualQuantity);
         Assertions.assertEquals(expectedPrice, actualPrice);
 
+    }
+
+    @Test
+    void findClothing() {
+        //Given
+        ClothingService clothingService = new ClothingService();
+        Clothing foundClothing = new Clothing();
+        clothingService.create("Shirt","Arrow",32,10,30);
+        clothingService.create("Jeans","Levis",32,8,42);
+        //When
+        foundClothing = clothingService.findClothing(2);
+        //Then
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    void findAll() {
+        //Given
+        ClothingService clothingService = new ClothingService();
+        //Clothing foundClothing = new Clothing();
+        clothingService.create("Shirt","Arrow",32,10,30);
+        clothingService.create("Jeans","Levis",32,8,42);
+        //When
+        Clothing [] clothing = clothingService.findAll();
+        //Then
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void delete() {
+        //Given
+        ClothingService clothingService = new ClothingService();
+        Boolean isClotheDeleted = true;
+        clothingService.create("Shirt","Arrow",32,10,30);
+        clothingService.create("Jeans","Levis",32,8,42);
+        //When
+        isClotheDeleted = clothingService.delete(2);
+        //Then
+        Assertions.assertEquals(true, isClotheDeleted);
     }
 }
