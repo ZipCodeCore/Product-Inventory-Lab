13f4a683019f074121d45e296f2d4f922ca83914
Manny <manny@ZCW-JLAT.home>
2021-07-04 16:29:28 -0400

    added hat properties

--- src/main/java/models/Hat.java
+++ src/main/java/models/Hat.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Hat {
+    private int id;
+    private String type; //fitted, snapback or strapback
+    private String sport; //baseball, basketball, NFL
+    private String team;
+    private int size;
+    private int quantity;
+    private float price;
 }
