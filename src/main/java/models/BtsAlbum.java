c3e3f27c8eecfb41324cdeae408c3724a9786f55
laura <laurab90>
2021-07-05 22:58:48 -0400

    the things

--- src/main/java/models/BtsAlbum.java
+++ src/main/java/models/BtsAlbum.java
@@ -1,4 +1,14 @@
 package models;
 
 public class BtsAlbum {
+
+    private int id;
+    private String name;
+    private String color;
+    private String concept;
+    private int size;
+    private int qty;
+    private float price;
+
+
 }
